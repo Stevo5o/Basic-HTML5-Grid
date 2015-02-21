540f250d7960799c25803ef9ac476b84702508a9
Stevo5o <stevo.joc@gmail.com>
2015-02-21 12:01:54 +0000

    Basic JS click event

---
+++ public_html/script.js
@@ -0,0 +1,19 @@
+/**
+ * script.js 
+ * @ Stephen O'Connor, Febuary 2015
+ * 
+ * Dependencies: 
+ * http://tristanedwards.me/sweetalert 
+ * 
+ */
+
+// immediately invoked anonymous function
+(function () {
+
+    document.getElementById('grid').addEventListener('click', function (e) {
+        console.log(e);
+    }, false);
+
+}()); // end immediately invoked anonymous function
+
+
