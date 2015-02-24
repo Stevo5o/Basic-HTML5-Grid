e76b85526d5ea82fd3c833e1a9b9b24f252d55c2
Stevo5o <stevo.joc@gmail.com>
2015-02-24 19:17:11 +0000

    Clean

--- public_html/script.js
+++ public_html/script.js
@@ -10,8 +10,20 @@
 // immediately invoked anonymous function
 (function () {
 
-    document.getElementById('grid').addEventListener('click', function (e) {
-        console.log(e);
+    document.querySelector('#grid').addEventListener('click', function (e) {
+            	//var proGit = e.target.id;
+           console.log(e.target);
+//       if (e.target.id === e.target.id) {
+////           console.log(this);
+////           console.log(e.target.id);
+////           
+//       //     // var howmany = this.querySelectorAll('li').length;
+//
+//      // console.dir(e.target.id);
+////        var removeTarget = e.target.id;
+//        // console.log(removeTarget);
+////        removeTarget.removeChild(removeTarget);
+//      }
     }, false);
 
 }()); // end immediately invoked anonymous function
